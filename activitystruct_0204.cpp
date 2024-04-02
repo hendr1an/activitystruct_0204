04341148b4bd7890701b9ae156fc11c929b364c8
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-04-02 15:55:03 +0700

    commit14

--- activitystruct_0204.cpp
+++ activitystruct_0204.cpp
@@ -16,16 +16,15 @@ struct Mahasiswa {
 int main(){
     Mahasiswa mhs;
     cout << "Nomor Mahasiswa : ";
-    cin >> mhs.nim;
-    cout <<"Nama Mahasiswa : ";
-    cin >> mhs.nama;
+    getline(cin , mhs.nim);
+    cout << "Nama Mahasiswa : ";
+    getline(cin,mhs.nama);
 
     cout << "Alamat Mahasiswa : "<<endl;
     cout << "\t Nama Desa : ";
     cin >> mhs.alamat.desa;
     cout << "\t Nama Kota : ";
     cin >> mhs.alamat.kota;
-
     cout << "Umur Mahasiswa : ";
     cin >> mhs.umur;
 
