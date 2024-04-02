8b1b118ef6858b35806ae4137416ab0c4d1b86a0
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-04-02 16:03:24 +0700

    commit15

--- activitystruct_0204.cpp
+++ activitystruct_0204.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 struct DetailAlamat{
@@ -6,7 +7,7 @@ struct DetailAlamat{
     string kota;
 };
 
-struct Mahasiswa {
+struct  Mahasiswa {
     string nim;
     string nama;
     DetailAlamat alamat;
@@ -17,22 +18,6 @@ int main(){
     Mahasiswa mhs;
     cout << "Nomor Mahasiswa : ";
     getline(cin , mhs.nim);
-    cout << "Nama Mahasiswa : ";
+    cout <<"Nama Mahasiswa : ";
     getline(cin,mhs.nama);
-
-    cout << "Alamat Mahasiswa : "<<endl;
-    cout << "\t Nama Desa : ";
-    cin >> mhs.alamat.desa;
-    cout << "\t Nama Kota : ";
-    cin >> mhs.alamat.kota;
-    cout << "Umur Mahasiswa : ";
-    cin >> mhs.umur;
-
-    cout << endl;
-    cout <<"\n NIM : "<<mhs.nim;
-    cout << "\n Nama : "<< mhs.nama;
-    cout << "\n Alamat : "; 
-    cout << "\n \t Desa : "<< mhs.alamat.desa;
-    cout << "\n \t Kota : "<< mhs.alamat.kota;
-    cout << "\n umur : "<< mhs.umur;
 }
