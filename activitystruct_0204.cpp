752cbc3c066fb3354861327e5c663e91c2f1511b
hendr1an <164998945+hendr1an@users.noreply.github.com>
2024-04-02 17:17:44 +0700

    commit21

--- activitystruct_0204.cpp
+++ activitystruct_0204.cpp
@@ -15,28 +15,12 @@ struct  Mahasiswa {
 };
 
 int main(){
-    Mahasiswa mhs [3];
+    Mahasiswa mhs[3];
     for (int i=0;i<3;i++){
+        cout <<"Data ke-"<<endl;
         cout << "Nomor Mahasiswa : ";
-        getline(cin , mhs.nim);
+        getline(cin , mhs[i].nim);
         cout <<"Nama Mahasiswa : ";
-        getline(cin,mhs.nama);
-
-        cout << "Alamat Mahasiswa : "<<endl;
-        cout << "\t Nama Desa : ";
-        cin >> mhs.alamat.desa;
-        cout << "\t Nama Kota : ";
-        cin >> mhs[i].alamat.kota;
-        cout << "Umur Mahasiswa : ";
-        cin >> mhs[i].umur;
-        cin.ignore(1,'\n');
-}
-for (int=0;i<3;i++){
-    cout << endl;
-    cout <<"\n NIM : "<<mhs[i].nim;
-    cout << "\n Alamat : "<< mhs[i].nama;
-    cout << "\n \t Desa : "<< mhs[i].alamat.desa;
-    cout << "\n \t Kota : "<< mhs[i].alamat.kota;
-    cout << "\n umur : "<< mhs[i].umur;
+        getline(cin,mhs[i].nama);
     }
 }
